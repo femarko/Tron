481401214cac1516db67137fd9ed5fcff3e5b22a
femarko <fenstermark7@gmail.com>
2025-03-28 19:13:02 +0100

    feat: src/service_layer/app_manager.py: save_address_info() added.

--- src/service_layer/app_manager.py
+++ src/service_layer/app_manager.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 
 import src.tron_interface as tr
+import src.domain.models as models
+from src.service_layer.unit_of_work import UnitOfWork
 
 
 def get_energy_and_bandwidth(addr: str, network: tr.Optional[tr.TronNetwork]) -> dict[str, int]:
@@ -11,5 +13,9 @@ def get_balance(addr: str, network: tr.Optional[tr.TronNetwork]) -> Decimal:
     tron_client = tr.create_tron_client(network=network)
     return tron_client.get_balance(addr=addr)
 
-def save_address_info(**data) -> None:
-    pass
+def save_address_info(data: dict[str, int], uow: UnitOfWork) -> int:
+    entry = models.create_addrbank_entry(**data)
+    with uow:
+        uow.address_repo.add(entry)
+        uow.commit()
+    return entry.id
