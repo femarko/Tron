651137a0d1bebfd9893ee4db52df3af58f600c34
femarko <fenstermark7@gmail.com>
2025-03-28 12:57:49 +0100

    feat: src/tron_interface.py modified.

--- src/tron_interface.py
+++ src/tron_interface.py
@@ -13,10 +13,14 @@ class TronClient:
     def __init__(self, network: TronNetwork = TronNetwork.NILE) -> None:
         self.client = tronpy.Tron(network=network)
 
-    def get_energy(self, addr: str) -> dict[str, int]:
+    def get_energy_and_bandwidth(self, addr: str) -> dict[str, int]:
         resources = self.client.get_account_resource(addr=addr)
         energy = max(resources.get("EnergyLimit", 0) - resources.get("EnergyUsed", 0), 0)
         bandwidth = max(
             resources.get("NetLimit", 0) - resources.get("NetUsed", 0) + resources.get("FreeNetLimit", 0), 0
         )
         return {"energy": energy, "bandwidth": bandwidth}
+
+    def get_balance(self, addr: str) -> int:
+        account_info = self.client.get_account_balance(addr=addr)
+        return account_info.get("balance", 0)
