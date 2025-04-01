7940f46b13d0ce77a52098668d2592301ef53541
femarko <fenstermark7@gmail.com>
2025-04-01 11:51:12 +0200

    feat: tests/test_web.py: test_get_info_from_db() added.

--- tests/test_web.py
+++ tests/test_web.py
@@ -17,5 +17,21 @@ def test_get_address_info(test_client):
     assert isinstance(response.json()["balance"], str)
 
 
-
-def test_get_info_from_db(): pass  # TODO: implement test
+def test_get_info_from_db(test_client):
+    response = test_client.get(url="https://127.0.0.1/get_info_from_db")
+    assert response.status_code == 200
+    # assert isinstance(response.json()["number"], int)
+    # assert response.json()["number"] > 0
+    assert isinstance(response.json()["page"], int)
+    assert response.json()["page"] > 0
+    assert isinstance(response.json()["per_page"], int)
+    assert response.json()["per_page"] > 0
+    assert isinstance(response.json()["total"], int)
+    assert response.json()["total"] > 0
+    assert isinstance(response.json()["total_pages"], int)
+    assert response.json()["total_pages"] > 0
+    assert isinstance(response.json()["items"][0]["balance"], str)
+    assert isinstance(response.json()["items"][0]["energy"], int)
+    assert response.json()["items"][0]["energy"] >= 0
+    assert isinstance(response.json()["items"][0]["bandwidth"], int)
+    assert response.json()["items"][0]["bandwidth"] >= 0
