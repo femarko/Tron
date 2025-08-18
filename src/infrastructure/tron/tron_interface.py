cbcc6734bf4a9a203f2e28cf3ebc12f385db3b48
femarko <fenstermark7@gmail.com>
2025-08-18 18:01:34 +0200

    refact: exception handling added into src/infrastructure/tron/tron_interface.py.

--- src/infrastructure/tron/tron_interface.py
+++ src/infrastructure/tron/tron_interface.py
@@ -1,16 +1,56 @@
 import tronpy
+from tronpy.exceptions import (
+    BadAddress,
+    BadKey,
+    BadSignature,
+    BadHash,
+    TaposError,
+    UnknownError,
+    TransactionError,
+    TvmError,
+    ValidationError,
+    ApiError,
+    NotFound,
+    AddressNotFound,
+    TransactionNotFound,
+    BlockNotFound,
+    AssetNotFound,
+    DoubleSpending,
+    BugInJavaTron
+)
 
 from decimal import Decimal
 from enum import Enum
 from typing import Optional
 
 from src.bootstrap.config import get_settings
+from src.domain.errors import TronError
 
 
 class TronNetwork(str, Enum):
     SHASTA = "shasta"
     NILE = "nile"
 
+
+TRON_ERRORS = (
+    BadAddress,
+    BadKey,
+    BadSignature,
+    BadHash,
+    TaposError,
+    UnknownError,
+    TransactionError,
+    TvmError,
+    ValidationError,
+    ApiError,
+    NotFound,
+    AddressNotFound,
+    TransactionNotFound,
+    BlockNotFound,
+    AssetNotFound,
+    DoubleSpending,
+    BugInJavaTron
+)
 
 class TronClient:
     def __init__(self, network: Optional[TronNetwork] = None) -> None:
@@ -20,15 +60,21 @@ class TronClient:
             self.client = tronpy.Tron()
 
     def get_energy_and_bandwidth(self, addr: str) -> dict[str, int]:
-        resources = self.client.get_account_resource(addr=addr)
-        energy = max(resources.get("EnergyLimit", 0) - resources.get("EnergyUsed", 0), 0)
-        bandwidth = max(
-            resources.get("NetLimit", 0) - resources.get("NetUsed", 0) + resources.get("freeNetLimit", 0), 0
-        )
-        return {"energy": energy, "bandwidth": bandwidth}
+        try:
+            resources = self.client.get_account_resource(addr=addr)
+            energy = max(resources.get("EnergyLimit", 0) - resources.get("EnergyUsed", 0), 0)
+            bandwidth = max(
+                resources.get("NetLimit", 0) - resources.get("NetUsed", 0) + resources.get("freeNetLimit", 0), 0
+            )
+            return {"energy": energy, "bandwidth": bandwidth}
+        except TRON_ERRORS as e:
+            raise TronError(message=str(e)) from e
 
     def get_balance(self, addr: str) -> Decimal:
-        return self.client.get_account_balance(addr=addr)
+        try:
+            return self.client.get_account_balance(addr=addr)
+        except TRON_ERRORS as e:
+            raise TronError(message=str(e)) from e
 
 
 def create_tron_client(mode: str) -> TronClient:
