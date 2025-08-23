7072f0a6774e5fca2596ed92c9645af86e83c563
femarko <fenstermark7@gmail.com>
2025-08-23 22:53:31 +0200

    feat: exceptions handling modified in tron interface.

--- src/infrastructure/tron/exceptions.py
+++ src/infrastructure/tron/exceptions.py
@@ -13,6 +13,11 @@ NOT_FOUND_TR_ERRORS = tuple(v for k, v in tron_exceptions.items() if k.endswith(
 BAD_DATA_TR_ERRORS = tuple(v for k, v in tron_exceptions.items() if k.endswith("Bad") or k.startswith("Bad"))
 OTHER_TR_ERRORS = tuple(v for k, v in tron_exceptions.items() if v not in (*NOT_FOUND_TR_ERRORS, *BAD_DATA_TR_ERRORS))
 
+tron_exceptions_types = {
+    "not_found": NOT_FOUND_TR_ERRORS,
+    "bad_data": BAD_DATA_TR_ERRORS,
+    "other": OTHER_TR_ERRORS
+}
 
 class TronError(InfrastructureError):
     def __init__(
@@ -24,9 +29,9 @@ class TronError(InfrastructureError):
         self.code = code
 
 
-def tron_exc_mapper(exc: Exception) -> Exception:
-    if isinstance(exc, NOT_FOUND_TR_ERRORS):
+def tron_exc_mapper(exc: Exception, exception_types: dict[str, tuple[Exception]]) -> Exception:
+    if isinstance(exc, exception_types.get("not_found")):
         return NotFoundError(message=str(exc))
-    if isinstance(exc, BAD_DATA_TR_ERRORS):
+    if isinstance(exc, exception_types.get("bad_data")):
         return BadRequestError(message=f"Wrong input data: {str(exc)}")
     return TronError(message=str(exc))
--- src/infrastructure/tron/tron_interface.py
+++ src/infrastructure/tron/tron_interface.py
@@ -7,25 +7,34 @@ from typing import (
     cast,
     Callable
 )
-
-from src.infrastructure.tron.exceptions import tron_exc_mapper
 from src.application.protocols import TronToolProto
-
+from src.infrastructure.tron.exceptions import (
+    TronError,
+    tron_exc_mapper,
+    tron_exceptions_types
+)
 
 class TronNetwork(str, Enum):
     SHASTA = "shasta"
     NILE = "nile"
+    MAINNET = "mainnet"
 
 
 class TronClient:
     def __init__(
             self,
             tron_tool: Callable[..., TronToolProto],
+            exception_types: dict[str, tuple[Exception]],
             network: Optional[TronNetwork] = None) -> None:
         if network:
+            try:
+                TronNetwork(network)
+            except ValueError:
+                raise TronError(message=f"Invalid network: {network}.")
             self.client = tron_tool(network=network)
         else:
             self.client = tron_tool()
+        self.exception_types = exception_types
 
     def get_energy_and_bandwidth(self, addr: str) -> dict[str, int]:
         try:
@@ -37,19 +46,23 @@ class TronClient:
             return {"energy": energy, "bandwidth": bandwidth}
         except Exception as e:
             print(f"From TronClient.get_energy_and_bandwidth - Exception: {e = }")  # todo: remove
-            raise tron_exc_mapper(exc=e) from e
+            raise tron_exc_mapper(exc=e, exception_types=self.exception_types) from e
 
     def get_balance(self, addr: str) -> Decimal:
         try:
             return self.client.get_account_balance(addr=addr)
         except Exception as e:
-            raise tron_exc_mapper(exc=e) from e
+            raise tron_exc_mapper(exc=e, exception_types=self.exception_types) from e
 
 
 def create_tron_client(mode: str) -> TronClient:
     if mode in {"test", "dev"}:
         return TronClient(
             tron_tool=cast(Type[TronToolProto], tronpy.Tron),
-            network=TronNetwork.NILE
+            network=TronNetwork.NILE,
+            exception_types=tron_exceptions_types
         )
-    return TronClient(tron_tool=cast(Type[TronToolProto], tronpy.Tron))
+    return TronClient(
+        tron_tool=cast(Type[TronToolProto], tronpy.Tron),
+        exception_types=tron_exceptions_types
+    )
